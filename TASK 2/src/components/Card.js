c1d0eb1c1770f7bb4c0b58d0cdc13f7e3b926b4b
Siddharthposhala <83816591+Siddharthposhala@users.noreply.github.com>
2022-11-26 19:45:33 +0530

    Add files via upload

---
+++ TASK 2/src/components/Card.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Users = ({loading,users}) => {
+    return loading ? (   
+          <div id="main">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="Loaading.." className="loader"/>
+          </div>
+        ) : 
+        (
+          <div id="main">
+    
+          {users.map(user =>
+                      <div className="project column3">
+                        {
+                          console.log(user)
+                        }
+                        <div className="profile">
+                          <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+                          <h1 className="name">{user.first_name} {user.last_name}</h1>
+                          <p className="email">{user.email}</p>
+                          <p>User ID: {user.id}</p>
+                        </div>
+                      </div>
+            )
+          }
+          </div>
+        )
+}
+
+export default Users;
